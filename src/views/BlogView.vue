63dc957038cd4690c742a9c68dac0b65b29732ba
adharaka <150102828+adharaka@users.noreply.github.com>
2025-07-28 09:50:05 +0700

    Update BlogView.vue

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -4,15 +4,15 @@ export default {
     return {
       activeTab: 1,
       items: [
-  {
-    id: 1,
-    name: 'Sertifikat Penghargaan PKL di PT. Tera Data Indonusa',
-    imageUrl: '/img/sertifikat1.jpg',
-    status: 'Saya mendapatkan sertifikat PKL saat saya magang di PT. Tera Data Indonusa yang memproduksi laptop lokal merk Axioo, disana saya jadi operator produksinya',
-    tech: 'Sertifikat PKL',
-    github: 'null',
-    demo: 'null'
-  },
+        {
+          id: 1,
+          name: 'Sertifikat Penghargaan PKL di PT. Tera Data Indonusa',
+          imageUrl: '/img/sertifikat1.jpg',
+          status: 'Saya mendapatkan sertifikat PKL saat saya magang di PT. Tera Data Indonusa yang memproduksi laptop lokal merk Axioo, disana saya jadi operator produksinya',
+          tech: 'Sertifikat PKL',
+          github: 'null',
+          demo: 'null'
+        },
         {
           id: 2,
           name: 'Makeblock',
@@ -98,8 +98,17 @@ export default {
           github: 'null',
           demo: 'null'
         },
-        {
+                {
           id: 12,
+          name: 'Sertifikat magang bea cukai Purwakarta',
+          imageUrl: '/img/beacukai.png',
+          tech: 'Kementrian Keuangan Republik Indonesia',
+          status: 'Saya mendapatkan sertifikat magang dari Kemenkeu',
+          github: 'null',
+          demo: 'null'
+        },
+        {
+          id: 13,
           name: 'Sertifikat Miktrotik - MTCNA',
           imageUrl: '/img/sertifikat12.jpg',
           tech: 'Mikrotik - MTCNA',
@@ -107,6 +116,15 @@ export default {
           github: 'null',
           demo: 'null'
         },
+        {
+          id: 14,
+          name: 'Sertifikat magang PT Indorama Synthetics Tbk',
+          imageUrl: '/img/indorama.jpg',
+          tech: 'PT Indorama Synthetics Tbk',
+          status: 'Saya mendapatkan sertifikat magang dari PT Indorama',
+          github: 'null',
+          demo: 'null'
+        }
       ]
     };
   }
@@ -126,21 +144,17 @@ export default {
       </header>
       <section>
         <div>
-          <div class="grid grid-cols-1 gap-4 pb-32 md:grid-cols-3 md:gap-3 xl:grid-cols-3 xl:gap-3 2xl:gap-5 fade-zoom-in">
+          <div
+            class="grid grid-cols-1 gap-4 pb-32 md:grid-cols-3 md:gap-3 xl:grid-cols-3 xl:gap-3 2xl:gap-5 fade-zoom-in">
             <div v-for="item in items" :key="item.id">
               <div
                 class="item-card flex flex-col items-center gap-2 rounded bg-[#1e1e1f] hover:bg-[#282828] border border-[#383838] rounded-xl text-amber-50 md:gap-3 px-5 py-5 lg:px-5 ">
                 <div class="flex h-12 w-12 items-center justify-center p-0 h-full w-full lg:p-0 zoom-in">
-  <img
-    :src="item.imageUrl"
-    :alt="item.name"
-    loading="lazy"
-    decoding="async"
-    class="drop-shadow-xl rounded rounded-xl"
-  />
-</div>
-<div class="w-full flex flex-col gap-2 items-center text-sm md:text-base lg:text-lg">
-  <div class="title-text font-medium text-secondary">{{ item.name }}</div>
+                  <img :src="item.imageUrl" :alt="item.name" loading="lazy" decoding="async"
+                    class="drop-shadow-xl rounded rounded-xl" />
+                </div>
+                <div class="w-full flex flex-col gap-2 items-center text-sm md:text-base lg:text-lg">
+                  <div class="title-text font-medium text-secondary">{{ item.name }}</div>
 
                   <div class="w-full text-left text-[10px] text-[#c1c1c1] md:text-xs lg:text-sm">
                     {{ item.status }}</div>
@@ -149,48 +163,52 @@ export default {
                   </div>
                   <div class="w-full flex justify-end">
                     <div class="flex cursor-pointer items-end gap-2 text-primary">
-                      <a v-if="item.github !== 'null'"
-                        :href="item.github" target="_blank" rel="noreferrer"
+                      <a v-if="item.github !== 'null'" :href="item.github" target="_blank" rel="noreferrer"
                         title="View github repository" class="transition-all hover:text-accent">
-                        <svg stroke="currentColor"
-                          fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
-                          height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                        <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24"
+                          stroke-linecap="round" stroke-linejoin="round" height="16" width="16"
+                          xmlns="http://www.w3.org/2000/svg">
                           <path
                             d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
                           </path>
                         </svg></a>
-                        <a v-if="item.demo !== 'null'" :href="item.demo" target="_blank" rel="noreferrer"
+                      <a v-if="item.demo !== 'null'" :href="item.demo" target="_blank" rel="noreferrer"
                         title="View finished project" class="transition-all hover:text-accent">
-                        <svg stroke="currentColor"
-                          fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
-                          height="18" width="18" xmlns="http://www.w3.org/2000/svg">
+                        <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24"
+                          stroke-linecap="round" stroke-linejoin="round" height="18" width="18"
+                          xmlns="http://www.w3.org/2000/svg">
                           <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                           <polyline points="15 3 21 3 21 9"></polyline>
-                        <line x1="10" y1="14" x2="21" y2="3"></line>
-                      </svg></a></div>
+                          <line x1="10" y1="14" x2="21" y2="3"></line>
+                        </svg></a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </article>
-</div></template>
+      </section>
+    </article>
+  </div>
+</template>
 
 <style>
 .item-card:hover {
   transition: transform 0.3s ease;
   transform: translateY(-8px);
 }
-svg:hover{
+
+svg:hover {
   stroke: #ffdb70;
 }
+
 @keyframes fadeZoomIn {
   0% {
     opacity: 0;
     transform: scale(0.5);
   }
+
   100% {
     opacity: 1;
     transform: scale(1);
